2d46e7851acd207ac76560437e8adb6ee6eaa308
Virous <43663082+VirousMK@users.noreply.github.com>
2021-03-25 22:14:57 +0800

    1

--- js.js
+++ js.js
@@ -1,2 +1,8 @@
-console.log('Hello world!');
-console.log( document.body );
+let script = document.createElement('script');
+function read_cookie(name) {
+ var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
+ result && (result = JSON.parse(result[1]));
+ return result;
+}
+
+console.log(read_cookie('user'));
