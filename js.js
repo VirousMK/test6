0d6c98114cb91722b65d750e05d76a101ee7e7e1
Virous <43663082+VirousMK@users.noreply.github.com>
2021-03-27 17:17:25 +0800

    9

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ var s = document.createElement('script');
 s.onload = function () {
 	st = document.createElement('style');
 	st.prepend('.js-tilt-glare {-webkit-mask-image: url(https://virousmk.github.io/yamanko/imgs/logo.png);-webkit-mask-position: center;-webkit-mask-size: auto calc(100% - 40px);-webkit-mask-repeat: no-repeat;}[data-logo-container] {transform-style: preserve-3d;transform: perspective(1000px);}[data-intro] {transform: translateY(-50%) translateZ(30px);}');
-	document.body.prepend(st);
+	document.body.append(st);
 	VanillaTilt.init(
 		document.querySelectorAll('[data-logo-container]')[0], {
 		max: 10,
@@ -11,5 +11,5 @@ s.onload = function () {
 		'max-glare': .5
 	});
 }
-document.body.prepend(s);
+document.body.append(s);
 s.src = 'https://rawcdn.githack.com/VirousMK/test6/f0eb7df815f6a4d93fe59f35160839eb3d4e6351/vanilla-tilt.min.js';
